6ac83f5771b6b1eaac12da6e83f08848e1237420
Fuad Tamton <fuadtamton@MacBook-Pro.local>
2022-04-14 12:10:20 +0530

    pagination added

--- app/controllers/searchSimilarPage.js
+++ app/controllers/searchSimilarPage.js
@@ -6,7 +6,7 @@ module.exports = async function searchSimilarPage(req, res) {
         if (!body.url) throw Error('URL is required');
         if (!body.type) throw Error('Type is required');
         const result = await searchSimilarPageES(body);
-        res.send({ data: result.hits.hits, status: 'Success' });
+        res.send({ total: result.hits.total.value, data: result.hits.hits, status: 'Success' });
 
     } catch (e) {
         console.log('error | ', e);

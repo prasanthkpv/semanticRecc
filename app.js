77e9a15f410faaf885ce01707bad8c88ed1e2d66
Fuad Tamton <fuadtamton@MacBook-Pro.local>
2022-04-19 20:54:00 +0530

    minor fix

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 8000;
 
 const RouterV1 = express.Router();
 
-RouterV1.use(cors());
-RouterV1.use(bodyParser.urlencoded());
-RouterV1.use(bodyParser.json());
 RouterV1.post('/searchPage', searchPage);
 RouterV1.post('/searchSimilarPages', searchSimilarPage);
 RouterV1.get('/getAllIndices', getAllIndices);
 
+app.use(cors());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use('/v1', RouterV1);
 
 app.listen(PORT);
